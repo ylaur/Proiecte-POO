f22b1c8dc7ac043310923574e39b4b9ba4cf7b20
Laur <224015109+ylaur@users.noreply.github.com>
2026-04-01 23:07:34 +0300

    Refactor Produs class constructors and operators

--- Produs.h
+++ Produs.h
@@ -2,13 +2,13 @@
 #include <iostream>
 #include <cstring>
 
-class Produs { // Un obiect de tip "Produs" are 2 date membre: Numele produsului si Pretul sau.
+class Produs {
     char *Nume;
     double Pret;
 
 public:
-    Produs() : Nume(NULL), Pret(0) {}; // Constructor fara parametrii
-    Produs(const char *nume, const double pret) : Pret(pret) { // Constructor cu 2 parametrii
+    Produs() : Nume(NULL), Pret(0) {};
+    Produs(const char *nume, const double pret) : Pret(pret) {
         if (nume != NULL) {
             Nume = new char[std::strlen(nume) + 1];
             std::strcpy(Nume, nume);
@@ -17,7 +17,7 @@ public:
             Nume = NULL;
         }
     }
-    Produs(const Produs &p) : Pret(p.Pret) { // Constructor de copiere
+    Produs(const Produs &p) : Pret(p.Pret) {
         if (p.Nume != NULL) {
             Nume = new char[std::strlen(p.Nume) + 1];
             std::strcpy(Nume, p.Nume);
@@ -27,8 +27,10 @@ public:
         }
     }
 
-    Produs& operator=(const Produs &p) { // Operator Overloading pentru "="
-        if (&p == this) return *this;
+    Produs& operator=(const Produs &p) {
+        if (&p == this) {
+            return *this;
+        }
         delete [] Nume;
         if (p.Nume != NULL) {
             Nume = new char[std::strlen(p.Nume) + 1];
@@ -41,10 +43,10 @@ public:
         return *this;
     }
 
-    const char* getNume() const { return Nume; } // GETTERI
+    const char* getNume() const { return Nume; }
     double getPret() const { return Pret; }
 
-    void setNume(const char *nume) { // SETTERI
+    void setNume(const char *nume) {
         delete [] Nume;
         if (nume != NULL) {
             Nume = new char[std::strlen(nume) + 1];
@@ -56,13 +58,13 @@ public:
     }
     void setPret(double pret) { Pret = pret; }
 
-    friend std::ostream& operator<<(std::ostream &out, const Produs &p) { // Afisare specifica obiectelor clasei "Produs"
+    friend std::ostream& operator<<(std::ostream &out, const Produs &p) {
         out << "Numele produsului -> " << (p.Nume == NULL || p.Nume[0] == '\0' ? "NULL" : p.Nume) << std::endl << "Pretul produsului -> "
         << p.Pret << std::endl;
         return out;
     }
 
-    ~Produs() { // Destructor
+    ~Produs() {
         if (Nume != NULL) {
             delete[] Nume;
         }
