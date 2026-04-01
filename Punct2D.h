c020b658f90dc7891759825102d97b8981de4a24
Laur <224015109+ylaur@users.noreply.github.com>
2026-04-01 23:07:52 +0300

    Refactor Punct2D class constructors and methods

--- Punct2D.h
+++ Punct2D.h
@@ -3,30 +3,30 @@
 #include <iomanip>
 #include <cmath>
 
-class Punct2D { // Un obiect de tip "Punct2D" are 2 date membre: coordonata X si coordonata Y.
+class Punct2D {
     unsigned int X, Y;
 
 public:
-    Punct2D() : X(0), Y(0) {}; // Constructor fara parametrii 
-    Punct2D(const unsigned int x, const unsigned int y) : X(x), Y(y) {}; // Constructor cu 2 parametrii
-    Punct2D(const Punct2D &p) { X = p.X; Y = p.Y; }; // Constructor de copiere
+    Punct2D() : X(0), Y(0) {};
+    Punct2D(const unsigned int x, const unsigned int y) : X(x), Y(y) {};
+    Punct2D(const Punct2D &p) { X = p.X; Y = p.Y; };
 
-    unsigned int getX() const { return this->X; } // GETTERI
+    unsigned int getX() const { return this->X; }
     unsigned int getY() const { return this->Y; }
 
-    void setX(const unsigned int x) { this->X = x; }; // SETTERI
+    void setX(const unsigned int x) { this->X = x; };
     void setY(const unsigned int y) { this->Y = y; };
 
-    double distanta(const Punct2D &p) const { // Metoda care returneaza distanta dintre 2 obiecte de tip "Punct2D"
+    double distanta(const Punct2D &p) const {
         const long long x = (long long)this->X - (long long)p.X;
         const long long y = (long long)this->Y - (long long)p.Y;
         return std::sqrt(x * x + y * y);
     }
 
-    friend std::ostream& operator<<(std::ostream &out, const Punct2D &p) { // Afisare specifica obiectelor clasei "Punct2D"
+    friend std::ostream& operator<<(std::ostream &out, const Punct2D &p) {
         out << std::setprecision(2) << std::fixed << "(" << p.X << ", " << p.Y << ")";
         return out;
     }
 
-    ~Punct2D() {}; // Destructor
+    ~Punct2D() {};
 };
