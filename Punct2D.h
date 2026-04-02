033c130fcce69a5a4264574599b5cda2b5125e37
Laur <224015109+ylaur@users.noreply.github.com>
2026-04-02 09:24:38 +0300

    Add comments in Romanian to Punct2D class
    Added Romanian comments to constructors, getters, setters, distance method, and operator overloading for clarity.

--- Punct2D.h
+++ Punct2D.h
@@ -3,30 +3,32 @@
 #include <iomanip>
 #include <cmath>
 
-class Punct2D {
-    unsigned int X, Y;
+class Punct2D { // Clasa Punct2D
+    unsigned int X, Y; // 2 Date Membre
 
 public:
-    Punct2D() : X(0), Y(0) {};
-    Punct2D(const unsigned int x, const unsigned int y) : X(x), Y(y) {};
-    Punct2D(const Punct2D &p) { X = p.X; Y = p.Y; };
+    Punct2D() : X(0), Y(0) {}; // Constructor fara parametrii
+    Punct2D(const unsigned int x, const unsigned int y) : X(x), Y(y) {}; // Constructor cu 2 Parametrii
+    Punct2D(const Punct2D &p) { X = p.X; Y = p.Y; }; // Constructor de Copiere
 
-    unsigned int getX() const { return this->X; }
-    unsigned int getY() const { return this->Y; }
+    // GETTERI
+    unsigned int getX() const { return this->X; } // Getter pentru X
+    unsigned int getY() const { return this->Y; } // Getter pentru Y
 
-    void setX(const unsigned int x) { this->X = x; };
-    void setY(const unsigned int y) { this->Y = y; };
+    // SETTERI
+    void setX(const unsigned int x) { this->X = x; }; // Setter pentru X
+    void setY(const unsigned int y) { this->Y = y; }; // Setter pentru Y
 
-    double distanta(const Punct2D &p) const {
-        const long long x = (long long)this->X - (long long)p.X;
+    double distanta(const Punct2D &p) const {                    // Metoda ce calculeaza distanta dintre 2 Obiecte de tip Punct2D 
+        const long long x = (long long)this->X - (long long)p.X; // Exemplu apelare: locatie1.distanta(locatie2)
         const long long y = (long long)this->Y - (long long)p.Y;
         return std::sqrt(x * x + y * y);
     }
 
-    friend std::ostream& operator<<(std::ostream &out, const Punct2D &p) {
+    friend std::ostream& operator<<(std::ostream &out, const Punct2D &p) { // Operator Overloading pentru afisare (<<)
         out << std::setprecision(2) << std::fixed << "(" << p.X << ", " << p.Y << ")";
         return out;
     }
 
-    ~Punct2D() {};
+    ~Punct2D() {}; // Destructor
 };
